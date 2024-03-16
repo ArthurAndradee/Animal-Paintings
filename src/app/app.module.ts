4b0fb59264afd2807610b269edaf053a6281ae3e
arthur andrade <arthurandrad.23@gmail.com>
2024-03-16 19:37:53 -0300

    Feat: App finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
     HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
+    CartModule,
   ],
   providers: [
     provideClientHydration(),
